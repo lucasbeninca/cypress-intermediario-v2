bc76a0da7976c4fd5618d14fcddbbb8014a793f4
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2023-10-16 23:00:48 -0300

    up

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://daasdasdalocalhost',
     env: {
       hideCredentials: true,
       requestMode: true,
